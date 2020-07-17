b006ce6115e780225f1e8ff5386be50ebae11925
kir_rik <kir-rik-90@yandex.ru>
2020-07-17 20:53:51 +0400

    more

---
+++ 3 - JS/functions.js
@@ -0,0 +1,11 @@
+function fnDeclared() {
+	console.log('function declaration')
+}
+
+var fn = function(a) {
+	console.log('function expression')
+}
+
+var arr = () => {
+	console.log('array function')
+}
